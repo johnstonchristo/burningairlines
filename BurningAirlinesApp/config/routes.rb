899cbb05d5ee1b09614649575b4b524499ca0c8d
johnstonchristo <johnstonchristo@gmail.com>
2017-05-17 11:10:27 +1000

    initial structure

---
+++ BurningAirlinesApp/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'pages#app'
+  get '/app' => 'pages#app'
+
+  resources :users
+  resources :reservations
+  resources :flights
+  resources :planes
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
