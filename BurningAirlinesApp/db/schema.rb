da5b80057a498efe6639bb666284b6c533647b33
fimac <fimccawley@gmail.com>
2017-05-17 15:28:52 +1000

    added associations and seeded

--- BurningAirlinesApp/db/schema.rb
+++ BurningAirlinesApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517014736) do
+ActiveRecord::Schema.define(version: 20170517052030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,11 @@ ActiveRecord::Schema.define(version: 20170517014736) do
     t.integer  "reservation_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users_flights", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "flight_id"
   end
 
 end
--- BurningAirlinesApp/db/seeds.rb
+++ BurningAirlinesApp/db/seeds.rb
@@ -20,19 +20,18 @@ puts Flight.count
 
 User.destroy_all
 
-u1 = User.create(name: 'Matt');
-u2 = User.create(name: 'Chris');
-u3 = User.create(name: 'Fiona');
-u4 = User.create(name: 'JJ');
+u1 = User.create(name: 'Matt')
+u2 = User.create(name: 'Chris')
+u3 = User.create(name: 'Fiona')
+u4 = User.create(name: 'JJ')
 
 puts User.count
 
-# Reservation.destroy_all
-#
-# r1 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f1.id, user_id: u1.id, row: 1, col: "A");
-# r2 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f2.id, user_id: u2.id, row: 2, col: "B");
-# r3 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f3.id, user_id: u3.id, row: 4, col: "C");
-# r4 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f1.id, user_id: u4.id, row: 4, col: "D");
-# r5 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f3.id, user_id: u5.id, row: 2, col: "C");
-#
-# Reservation.count
+Reservation.destroy_all
+
+r1 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f1.id, user_id: u1.id, row: 1, col: "A");
+r2 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f2.id, user_id: u2.id, row: 2, col: "B");
+r3 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f3.id, user_id: u3.id, row: 4, col: "C");
+r4 = Reservation.create(date: DateTime.parse("03/05/2017 13:00"), flight_id: f1.id, user_id: u4.id, row: 4, col: "D");
+
+p Reservation.count
